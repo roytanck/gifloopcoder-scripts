b06d2b91bf3329505db87c3e6ffd7d6eefd50eee
Roy Tanck <roy.tanck@gmail.com>
2015-11-26 14:16:44 +0100

    Added a first attempt at working with Bezier curves.

---
+++ code/roy-stars.js
@@ -0,0 +1,45 @@
+function onGLC(glc) {
+	glc.loop();
+	// glc.playOnce();
+	glc.size(400, 400);
+	glc.setDuration(2);
+	glc.setFPS(30);
+	glc.setMode("single");
+	glc.setEasing(false);
+	glc.setMaxColors(256);
+	var list = glc.renderList,
+		width = glc.w,
+		height = glc.h,
+		color = glc.color;
+
+	// your code goes here:
+	var nr = 7;
+	var radius = 120;
+	var count = 0;
+
+	function getXPos( t ){
+		var x = Math.sin( ( (t+count)*2*Math.PI)/nr ) * radius;
+		return width/2 + x;
+	}
+
+	function getYPos( t ){
+		var y = -Math.cos( ( (t+count)*2*Math.PI)/nr ) * radius;
+		count++;
+		count = count%nr;
+		return height/2 + y;
+	}
+
+	for( var i=0; i<nr; i++ ){
+		list.addStar({
+			x: getXPos,
+			y: getYPos,
+			points: 5,
+			innerRadius: 20,
+			outerRadius: 50,
+			rotation: [ 0, 360/2.5 ],
+			fill: true,
+			stroke: false,
+			fillStyle: [ color.hsv( Math.floor( i*(360/nr) ), 1, 1 ), color.hsv( Math.floor( (i+1)*(360/nr) ), 1, 1 ) ]
+		});
+	}
+}
--- code/roy.js
+++ code/roy.js
@@ -2,10 +2,10 @@ function onGLC(glc) {
 	glc.loop();
 	// glc.playOnce();
 	glc.size(400, 400);
-	glc.setDuration(2);
+	glc.setDuration(3);
 	glc.setFPS(30);
-	glc.setMode("single");
-	glc.setEasing(false);
+	//glc.setMode("single");
+	//glc.setEasing(false);
 	glc.setMaxColors(256);
 	var list = glc.renderList,
 		width = glc.w,
@@ -13,35 +13,47 @@ function onGLC(glc) {
 		color = glc.color;
 
 	// your code goes here:
-	var nr = 7;
-	var radius = 120;
-	var count = 0;
-
-	function getXPos( t ){
-		var x = Math.sin( ( (t+count)*2*Math.PI)/nr ) * radius;
-		return width/2 + x;
-	}
-
-	function getYPos( t ){
-		var y = -Math.cos( ( (t+count)*2*Math.PI)/nr ) * radius;
-		count++;
-		count = count%nr;
-		return height/2 + y;
-	}
+	var nr = 5;
+	var amp = 2;
+	var counter = 0;
 
 	for( var i=0; i<nr; i++ ){
-		list.addStar({
-			x: getXPos,
-			y: getYPos,
-			points: 5,
-			innerRadius: 20,
-			outerRadius: 50,
-			rotation: [ 0, 360/2.5 ],
-			fill: true,
-			stroke: false,
-			fillStyle: [ color.hsv( Math.floor( i*(360/nr) ), 1, 1 ), color.hsv( Math.floor( (i+1)*(360/nr) ), 1, 1 ) ],
-			/*fillStyle: '#ffcc00',*/
-			/*phase: i/nr*/
+		list.addBezierCurve({
+			x0: width/2,
+			y0: height/2,
+			x1: getInnerX,
+			y1: getInnerY,
+			x2: width/2 + Math.sin( i*(2*Math.PI)/nr ) * 200,
+			y2: height/2 + -Math.cos( i*(2*Math.PI)/nr ) * 200,
+			x3: getOuterX,
+			y3: getOuterY,
+			fill: false,
+			lineWidth: 2,
+			strokeStyle: '#000000'
 		});
+	}
+
+
+	function getInnerX( t ){
+		var progress = counter + ( (t-0.5) * 2 ) * amp;
+		return width/2 + Math.sin( progress*(2*Math.PI)/nr ) * 10;
+	}
+
+	function getInnerY( t ){
+		var progress = counter + ( (t-0.5) * 2 ) * amp;
+		return width/2 + -Math.cos( progress*(2*Math.PI)/nr ) * 10;
+	}
+
+	function getOuterX( t ){
+		var progress = counter + ( (t-0.5) * 2 ) * amp;
+		return width/2 + Math.sin( progress*(2*Math.PI)/nr ) * 300;
+	}
+
+	function getOuterY( t ){
+		var progress = counter + ( (t-0.5) * 2 ) * amp;
+		var y = height/2 + -Math.cos( progress*(2*Math.PI)/nr ) * 300;
+		counter++;
+		counter = counter%nr;
+		return y;
 	}
 }
