94798117d383d78445ba6e807464861059b0e18c
Roy Tanck <roy.tanck@gmail.com>
2015-12-04 13:25:48 +0100

    Started work on a new soap-like effect.

--- code/roy.js
+++ code/roy.js
@@ -2,25 +2,24 @@ function onGLC(glc) {
 	glc.loop();
 	// glc.playOnce();
 	glc.size( 400, 400 );
-	glc.setDuration( 10 );
+	glc.setDuration( 8 );
 	glc.setFPS( 25 );
 	glc.setMode( "single" );
 	glc.setEasing( false );
-	glc.setMaxColors( 16 );
-	glc.setQuality( 10 );
+	glc.setMaxColors( 64 );
+	glc.setQuality( 1 );
 	var list = glc.renderList,
 		width = glc.w,
 		height = glc.h,
 		color = glc.color;
 
 	// your code goes here:
-	glc.styles.backgroundColor = color.rgb( 0, 50, 100 );
-	var segments = 5;
-	var segmentLength = 40;
-	var lastx0 = 0;
-	var lasty0 = 0;
+	glc.styles.backgroundColor = color.rgb( 60, 80, 120 );
+	var segments = 3;
+	var segmentLength = 50;
 	var lastx1 = 0;
 	var lasty1 = 0;
+	var lastnr = 0;
 	var arms = 17;
 
 	for( var arm = 0; arm < arms; arm++ ){
@@ -31,69 +30,53 @@ function onGLC(glc) {
 
 			list.addLine({
 				x0: function( t ){
-					if( this.nr == 0 ){
-						newx0 = width/2;
-					} else {
-						newx0 = lastx1;
-					}
-					lastx0 = newx0;
-					return newx0;
+					return ( this.nr == 0 ) ? width/2 : lastx1;
 				},
 				y0: function( t ){
-					if( this.nr == 0 ){
-						newy0 = width/2;
-					} else {
-						newy0 = lasty1;
-					}
-					lasty0 = newy0;
-					return newy0;
+					return ( this.nr == 0 ) ? height/2 : lasty1;
 				},
 				x1: function( t ){
 					if( this.nr ==0 ){
-						var newx1 = width/2 + Math.sin( ((this.arm/arms)+t)*2*Math.PI ) * seg;
+						var newx1 = width/2 + Math.sin( ((this.arm/arms)+t)*2*Math.PI ) * this.seg;
 					} else {
-						var newx1 = lastx1 + Math.sin( (this.nr+1)*((this.arm/arms)+t)*2*Math.PI ) * seg;
+						var newx1 = lastx1 + Math.sin( (this.nr+1)*((this.arm/arms)+t)*2*Math.PI ) * this.seg;
 					}
 					lastx1 = newx1;
 					return newx1;
 				},
 				y1: function( t ){
 					if( this.nr == 0 ){
-						var newy1 = height/2 + -Math.cos( ((this.arm/arms)+t)*2*Math.PI ) * seg;
+						var newy1 = height/2 + -Math.cos( ((this.arm/arms)+t)*2*Math.PI ) * this.seg;
 					} else {
-						var newy1 = lasty1 + -Math.cos( (this.nr+1)*((this.arm/arms)+t)*2*Math.PI ) * seg;
+						var newy1 = lasty1 + -Math.cos( (this.nr+1)*((this.arm/arms)+t)*2*Math.PI ) * this.seg;
 					}
 					lasty1 = newy1;
+					lastnr = this.nr;
 					return newy1;
 				},
 				lineWidth: 1.5,
-				strokeStyle: color.rgba( 255, 255, 255, 0.3 ),
+				strokeStyle: color.rgba( 255, 255, 255, 0 ),
 				nr: i,
 				arm: arm,
-				seg: seg,
+				seg: seg - ( (i/segments) * seg ) / 2,
 				phase: Math.random()
+			});
+
+			list.addCircle({
+				x: function( t ){
+					return lastx1;
+				},
+				y: function( t ){
+					return lasty1;
+				},
+				radius: function( t ){
+					return 3 + ( segments - lastnr ) * 8;
+				},
+				fillStyle: color.rgba( 220, 255, 255, 0.15 )
 			});
 
 		}
 
-		list.addCircle({
-			x: function( t ){
-				return lastx1;
-			},
-			y: function( t ){
-				return lasty1;
-			},
-			radius: 4,
-			fillStyle: color.rgba( 255, 255, 255, 1 )
-		});
-
 	}
-
-	list.addCircle({
-		x: width/2,
-		y: height/2,
-		radius: 4,
-		fillStyle: color.rgba( 255, 255, 255, 1 )
-	});
 
 }
