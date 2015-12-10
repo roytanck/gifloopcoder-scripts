f8f487800de21846000ab44f562e3b0e324385dc
Roy Tanck <roy.tanck@gmail.com>
2015-12-10 14:14:36 +0100

    Removed leftover parts from previous anim that were no longer used.

--- code/roy-wavelines.js
+++ code/roy-wavelines.js
@@ -25,11 +25,11 @@ function onGLC(glc) {
 				list.addLine({
 					x0: i * ( width/nr ) - (width/nr)/3,
 					y0: function( t ){
-						return getY( t, this.nr-0.1, 0, false );
+						return getY( t, this.nr-0.1 );
 					},
 					x1: i * ( width/nr ) + (width/nr)/3,
 					y1: function( t ){
-						return getY( t, this.nr+0.1, 0, false );
+						return getY( t, this.nr+0.1 );
 					},
 					stroke: true,
 					strokeStyle: color.rgba( 255, 255, 255, 0.6 ),
@@ -43,14 +43,9 @@ function onGLC(glc) {
 
 	}
 
-	function getY( t, i, offset, reverse ){
-		var currentY = height/2 + Math.sin( ( t + ( i / nr ) ) * 2 * Math.PI ) * 100;
-		var offsetY = height/2 + Math.sin( ( t + ( (i + offset ) / nr ) ) * 2 * Math.PI ) * 100;
-		if( reverse ){
-			return currentY - ( offsetY - currentY )/5;
-		}
-		return offsetY;
+	function getY( t, i ){
+		var y = height/2 + Math.sin( ( t + ( i / nr ) ) * 2 * Math.PI ) * 100;
+		return y;
 	}
-
 
 }
