1ad0d86ada8f5e32ffed2b3fbfcbbc5b074f1876
Roy Tanck <roy.tanck@gmail.com>
2015-12-11 12:07:32 +0100

    Early sketch of a possible GLC logo design made with GLC.

--- code/roy.js
+++ code/roy.js
@@ -1,8 +1,8 @@
 function onGLC(glc) {
 	glc.loop();
-	glc.size( 400, 400 );
+	glc.size( 500, 300 );
 	glc.setDuration( 4 );
-	glc.setFPS( 25 );
+	glc.setFPS( 30 );
 	glc.setMode( "single" );
 	glc.setEasing( false );
 	glc.setMaxColors( 256 );
@@ -15,38 +15,49 @@ function onGLC(glc) {
 	// your code goes here:
 	glc.styles.backgroundColor = color.rgba( 255, 255, 255, 1 );
 
-	var cols = [ '#d00', '#0d0', '#00d' ];
-	var clusters = 5;
-	var clusterRadius = 280;
-	var nr = cols.length;
+	var strs = [ 'GIF', 'LOOP', 'CODER' ];
+	var counter = 0;
+	var letters = 12; // calculate this to make it more flexible?
+	var spacing = 100;
 
-	for( var c=0; c<clusters; c++ ){
-		var bx = width/2 + Math.sin( (c/clusters)*2*Math.PI ) * clusterRadius;
-		var by = height/2 + -Math.cos( (c/clusters)*2*Math.PI ) * clusterRadius;
+	for( var s=0; s<strs.length; s++ ){
+
+		var nr = strs[s].length;
 
 		for( var i=0; i<nr; i++ ){
 
-			list.addCircle({
-				x: function( t ){
-					return this.bx + Math.sin( (t+this.nr/nr)*2*Math.PI ) * ( Math.sin(t*2*Math.PI) * this.r );
-				},
-				y: function( t ){
-					return this.by - Math.cos( (t+this.nr/nr)*2*Math.PI ) * ( Math.sin(t*2*Math.PI) * this.r );
-				},
-				radius: 200,
+			list.addRect({
+				x: i * spacing + spacing/2,
+				y: s * spacing + spacing/2,
+				w: spacing,
+				h: spacing,
 				stroke: false,
 				fill: true,
-				fillStyle: cols[ i ],
-				blendMode: 'darker',
-				nr: i,
-				r: 60,
-				bx: bx,
-				by: by,
-				phase: c/clusters
+				fillStyle: color.hsv( (counter/letters)*360, 1, 0.75 )
 			});
+
+			list.addText({
+				text: strs[s].charAt( i % nr ),
+				x: i * spacing + spacing/2,
+				y: s * spacing + spacing/2 - spacing/25,
+				/*translationX: function( t ){ return Math.sin( t*2*Math.PI ) * spacing/2.8 + spacing/2.8; },*/
+				/*rotation: [ 0, 360 ],*/
+				stroke: false,
+				fill: true,
+				fillStyle: 'white',
+				fontSize: 70,
+				fontWeight: 'bold',
+				fontFamily: 'Roboto Condensed',
+				fontStyle: 'normal',
+				phase: 1 - ( counter / letters ),
+				nr: counter
+			});
+
+			counter++;
 
 		}
 
 	}
+
 
 }
