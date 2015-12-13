fa5247ad9da95cea9e8f89f6971598f645c571cb
Roy Tanck <roy.tanck@gmail.com>
2015-12-13 14:27:27 +0100

    Some more fun with blend modes.

--- code/roy-semicircles.js
+++ code/roy-semicircles.js
@@ -1,7 +1,7 @@
 function onGLC(glc) {
 	glc.loop();
 	glc.size( 400, 400 );
-	glc.setDuration( 4 );
+	glc.setDuration( 5 );
 	glc.setFPS( 25 );
 	glc.setMode( "single" );
 	glc.setEasing( false );
@@ -13,17 +13,17 @@ function onGLC(glc) {
 		color = glc.color;
 
 	// your code goes here:
-	glc.styles.backgroundColor = color.rgba( 0, 0, 0, 1 );
+	glc.styles.backgroundColor = color.rgba( 51, 51, 51, 1 );
 
-	var grid = 3;
+	var grid = 4;
 	var padding = 20;
 	var gridSize = ( width - ( 2*padding ) ) /grid;
-	var cols = [ '#f00', '#0f0', '#00f' ];
+	var cols = [ '#c00', '#0c0', '#00c' ];
 
 	for( var x=0; x<grid; x++ ){
 		for( var y=0; y<grid; y++ ){
 
-			var speeds = [1,2,3];
+			var speeds = [1,2,3,4];
 			speeds.sort( function(){ return 0.5 - Math.random(); } );
 
 			for( var c=0; c<cols.length; c++ ){
@@ -35,14 +35,12 @@ function onGLC(glc) {
 					drawFromCenter: true,
 					startAngle: 0,
 					endAngle: 180,
-					rotation: function( t ){
-						return Math.sin( t*2*Math.PI ) * ( this.speed * 10 );
-					},
+					rotation: [ 0, 360 ],
 					stroke: false,
 					fill: true,
 					fillStyle: cols[c],
 					blendMode: 'lighter',
-					speed: ( Math.random() < 0.5 ) ? speeds[c] : -speeds[c]
+					speedMult: speeds[c]
 				});
 
 			}
