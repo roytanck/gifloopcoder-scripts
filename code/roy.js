2c38158423da792de4a8e3a57c89bc9ed70eaf3d
Roy Tanck <roy.tanck@gmail.com>
2015-12-14 17:41:20 +0100

    Add some bouncing balls.

--- code/roy.js
+++ code/roy.js
@@ -1,11 +1,11 @@
 function onGLC(glc) {
 	glc.loop();
-	glc.size( 400, 400 );
-	glc.setDuration( 3 );
-	glc.setFPS( 25 );
-	glc.setMode( "single" );
+	glc.size( 600, 400 );
+	glc.setDuration( 1 );
+	glc.setFPS( 30 );
+	//glc.setMode( "single" );
 	glc.setEasing( false );
-	glc.setMaxColors( 128 );
+	glc.setMaxColors( 256 );
 	glc.setQuality( 1 );
 	var list = glc.renderList,
 		width = glc.w,
@@ -13,75 +13,70 @@ function onGLC(glc) {
 		color = glc.color;
 
 	// your code goes here:
-	glc.styles.backgroundColor = color.rgba( 0, 40, 10, 1 );
-	var lines = 50;
-	var targets = 11;
-	var rad = width/2 - 20;
+	glc.styles.backgroundColor = color.rgba( 80, 80, 160, 1 );
+	var nr = 3;
+	var startX = 150;
+	var dist = 150;
 
-	// grid
-	list.addGrid({
-		x: 0,
-		y: 0,
-		w: width,
-		h: height,
-		gridSize: 20,
-		lineWidth: 1,
-		strokeStyle: color.rgba( 150, 255, 200, 0.3 )
-	});
+	var gradient = color.createLinearGradient( 0, -height/2, 0, height/2 );
+    gradient.addColorStop( 0, '#112' );
+    gradient.addColorStop( 1, '#66a' );
 
-	// axis lines
-	for( var i=0; i<2; i++ ){
-		list.addLine({
-			x0: i ? 0 : width/2,
-			y0: i ? height/2 : 0,
-			x1: i ? width : width/2,
-			y1: i ? height/2 : height,
-			lineWidth: 1,
-			strokeStyle: color.rgba( 150, 255, 200, 0.4 )
-		});
-	}
+    list.addRect({
+    	x: width/2,
+    	y: height/2,
+    	w: width,
+    	h: height,
+    	stroke: false,	
+    	fillStyle: gradient
+    });
 
-	// outer circle
-	list.addCircle({
-		x: width/2,
-		y: height/2,
-		radius: rad,
-		fill: false,
-		stroke: true,
-		strokeStyle: color.rgba( 150, 255, 200, 1 ),
-		lineWidth: 1.5
-	});
+	for( var i=0; i<nr; i++ ){
 
-	// bleep, bleep
-	for( var i=0; i<lines; i++ ){
-		list.addRay({
-			x: width/2,
-			y: height/2,
-			length: rad,
-			angle: [ 0, 360 ],
-			lineWidth: 3,
-			strokeStyle: color.rgba( 150, 255, 200, (1-(i/lines))*0.8 ),
-			phase: 1 - i/360
-		});
-	}
+		var phase = Math.random();
 
-	// targets
-	for( var i=0; i<targets; i++ ){
-		var angle = Math.random() * ( 2 * Math.PI );
-		var dist = 40 + Math.random() * ( rad - 60 );
-		list.addCircle({
-			x: width/2 + Math.sin( angle ) * dist,
-			y: height/2 - Math.cos( angle ) * dist,
-			radius: [ 6, 2 ],
+		var shadow = list.addOval({
+			x: function( t ){
+				return ( startX + this.nr*dist + 10 ) + Math.sin( t*Math.PI/2 ) * 120;
+			},
+			y: function( t ){
+				return ( height*0.75 + 30 ) - Math.sin( t*Math.PI/2 ) * 50;
+			},
 			fill: true,
-			fillStyle: [ color.rgba( 150, 255, 200, 1 ), color.rgba( 150, 255, 200, 0 ) ],
-			stroke: false,
-			shadowColor: [ color.rgba( 150, 255, 200, 1 ), color.rgba( 150, 255, 200, 0 ) ],
+			fillStyle: function( t ){
+				return color.rgba( 0, 0, 0, 0.2 - Math.sin( t*Math.PI/2 ) * 0.15 );
+			},
+			rx: function( t ){
+				return 40 + Math.sin( t*Math.PI/2 ) * 15;
+			},
+			ry: function( t ){
+				return 15 + Math.sin( t*Math.PI/2 ) * 5;
+			},
+			shadowColor: color.rgba( 0, 0, 0, 1 ),
 			shadowOffsetX: 0,
 			shadowOffsetY: 0,
-			shadowBlur: 10,
-			phase: 1 - ( ( angle - Math.PI/2.05 )  / ( 2 * Math.PI ) ),
+			shadowBlur: 20,
+			nr: i,
+			phase: i/nr
 		});
+
+		var gradient = color.createRadialGradient( -18, -22, 0, -36, -44, 85 );
+		gradient.addColorStop( 0, 'white' );
+		gradient.addColorStop( 0.6, '#66a' );
+		gradient.addColorStop( 0.9, '#000' );
+		gradient.addColorStop( 1, '#224' );
+
+		var ball = list.addCircle({
+			x: startX + i * dist,
+			y: function( t ){ return height*0.75 - Math.abs( Math.sin( t*Math.PI/2 ) * height/2 ); },
+			radius: 40,
+			stroke: false,
+			fille: true,
+			fillStyle: gradient,
+			nr: i,
+			phase: i/nr
+		});
+
 	}
 
 }
