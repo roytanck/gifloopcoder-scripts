b738489e20359875ee2c6a5cb357affa72137564
Roy Tanck <roy.tanck@gmail.com>
2015-12-23 05:00:35 +0100

    Refactored the present anim, added fade in/out.

--- code/roy-present.js
+++ code/roy-present.js
@@ -1,10 +1,10 @@
 function onGLC(glc) {
 	glc.loop();
-	glc.size(400, 400);
-	glc.setDuration(5);
-	glc.setFPS(25);
-//     glc.setMode('single');
-//     glc.setEasing(false);
+	glc.size( 400, 400 );
+	glc.setDuration( 5 );
+	glc.setFPS( 25 );
+	// glc.setMode('single');
+	// glc.setEasing(false);
 	glc.setMaxColors( 256 );
 	glc.setQuality( 1 );
 	var list = glc.renderList,
@@ -14,178 +14,90 @@ function onGLC(glc) {
 
 	// your code goes here:
 	glc.styles.backgroundColor = color.rgba( 30, 50, 70, 1 );
-	var lw = 8;
-	var lw2 = 8;
-	var col1 = color.rgba( 40, 180, 40, 1 );
+	var lw1 = 7;
+	var lw2 = 7;
+	var col1 = color.rgba( 40, 180, 60, 1 );
 	var col2 = color.rgba( 255, 60, 40, 1 );
 
-	// box outlines
-	list.addSegment({
-		x0: width/2 - 120,
-		y0: height/2 - 80,
-		x1: width/2 - 120,
-		y1: height/2 + 80,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160,
+	var c = list.addContainer({
+		x: width/2,
+		y: height/2 + 10,
 	});
 	
-	list.addSegment({
-		x0: width/2 + 120,
-		y0: height/2 + 80,
-		x1: width/2 + 120,
-		y1: height/2 - 80,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
+	// box outlines
+	var lines1 = [
+		[ -120, -80, -120, 80 ],
+		[ 120, 80, 120, -80 ],
+		[ -120, -80, 0, -120 ],
+		[ 120, -80, 0, -120 ],
+		[ -120, 80, 0, 120 ],
+		[ 120, 80, 0, 120 ],
+		[ -120, -80, 0, -40 ],
+		[ 120, -80, 0, -40 ],
+		[ 0, -40, 0, 120 ]
+	];
 
-	list.addSegment({
-		x0: width/2 - 120,
-		y0: height/2 - 80,
-		x1: width/2,
-		y1: height/2 - 120,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 + 120,
-		y0: height/2 - 80,
-		x1: width/2,
-		y1: height/2 - 120,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 - 120,
-		y0: height/2 + 80,
-		x1: width/2,
-		y1: height/2 + 120,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 + 120,
-		y0: height/2 + 80,
-		x1: width/2,
-		y1: height/2 + 120,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x1: width/2 - 120,
-		y1: height/2 - 80,
-		x0: width/2,
-		y0: height/2 - 40,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 + 120,
-		y0: height/2 - 80,
-		x1: width/2,
-		y1: height/2 -40,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2,
-		y0: height/2 - 40,
-		x1: width/2,
-		y1: height/2 + 120,
-		lineWidth: lw,
-		stroke: true,
-		strokeStyle: col1,
-		segmentLength: 160
-	});
+	for( var i=0; i<lines1.length; i++ ){
+		list.addSegment({
+			x0: lines1[i][0],
+			y0: lines1[i][1],
+			x1: lines1[i][2],
+			y1: lines1[i][3],
+			lineWidth: lw1,
+			stroke: true,
+			strokeStyle: col1,
+			segmentLength: 160,
+			parent: c,
+			globalAlpha: getAlpha
+		});
+	}
 
 	// ribbon
+	var lines2 = [
+		[ 60, -60, -60, -100 ],
+		[ 60, -100, -60, -60 ],
+		[ -60, -60, -60, 100 ],
+		[ 60, 100, 60, -60 ]
+	];
+		
+	for( var i=0; i<lines2.length; i++ ){
+		list.addSegment({
+			x0: lines2[i][0],
+			y0: lines2[i][1],
+			x1: lines2[i][2],
+			y1: lines2[i][3],
+			lineWidth: lw2,
+			stroke: true,
+			strokeStyle: col2,
+			segmentLength: 160,
+			parent: c,
+			globalAlpha: getAlpha
+		});
+	}
 	
-	list.addSegment({
-		x0: width/2 + 60,
-		y0: height/2 - 60,
-		x1: width/2 - 60,
-		y1: height/2 - 100,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 + 60,
-		y0: height/2 - 100,
-		x1: width/2 - 60,
-		y1: height/2 - 60,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		segmentLength: 160
-	});
-	
-	list.addSegment({
-		x0: width/2 -60,
-		y0: height/2 - 60,
-		x1: width/2 - 60,
-		y1: height/2 + 100,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		segmentLength: 160
-	});
-
-	list.addSegment({
-		x0: width/2 + 60,
-		y0: height/2 + 100,
-		x1: width/2 + 60,
-		y1: height/2 - 60,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		segmentLength: 160
-	});
-
 	// knot
-	list.addArcSegment({
-		x: width/2+10,
-		y: height/2 - 116,
-		radius: 30,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		rotation: 90,
-		arc: [0,720]
-	});
+	var arcs = [
+		[ 10, -116, 90 ],
+		[ -10, -110, -90 ]
+	];
+	
+	for( var i=0; i<arcs.length; i++ ){
+		list.addArcSegment({
+			x: arcs[i][0],
+			y: arcs[i][1],
+			radius: 30,
+			lineWidth: lw2,
+			stroke: true,
+			strokeStyle: col2,
+			rotation: arcs[i][2],
+			arc: [0,720],
+			parent: c,
+			globalAlpha: getAlpha
+		});        
+	}
 
-	list.addArcSegment({
-		x: width/2-10,
-		y: height/2 - 110,
-		radius: 30,
-		lineWidth: lw2,
-		stroke: true,
-		strokeStyle: col2,
-		rotation: -90,
-		arc: [0,720]
-	});
-
+	function getAlpha( t ){
+		return Math.min( 1, Math.sin( t*Math.PI )*2 );
+	}
+	
 }
