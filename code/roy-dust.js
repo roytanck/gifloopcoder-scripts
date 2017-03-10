bff25f970ccd02714143745427daafc4094f5a9c
Roy Tanck <roy.tanck@gmail.com>
2017-03-10 20:09:12 +0100

    Added the 'dust' anim, which ended up looking more like sparks.

---
+++ code/roy-dust.js
@@ -0,0 +1,52 @@
+function onGLC(glc) {
+    glc.loop();
+    glc.size( 600, 600 );
+    glc.setDuration( 4 );
+    glc.setFPS( 25 );
+    glc.setMode('single');
+    glc.setEasing( false );
+    var list = glc.renderList,
+        width = glc.w,
+        height = glc.h,
+        color = glc.color;
+
+    // your code goes here:
+    glc.styles.backgroundColor = '#333';
+	var nr = 1000,
+        r = 140,
+        r2 = 450,
+        f = 2,
+        size= 6;
+	
+    for( var i=0; i<nr; i++ ){
+
+        var sx = Math.sin( (i/nr)*2*Math.PI ) * r;
+        var sy = Math.cos( (i/nr)*2*Math.PI ) * r
+        var dist = Math.random() * r2;
+        var angle = Math.random() * ( 2*Math.PI );
+        var tx = sx + Math.sin( angle ) * dist;
+        var ty = sy - Math.cos( angle ) * dist;
+        
+        list.addCircle({
+            _sx: sx,
+            _sy: sy,
+            _tx: tx,
+            _ty: ty,
+            x: [ (width/2)+(sx+(0.025*tx)), (width/2)+tx ],
+            y: [ (height/2)+(sy+(0.025*ty)), (height/2)+ty ],
+            radius: function(t){
+                if( t <= (1/f) ){
+                    return size - (t*(size*f));
+                } else {
+                    return 0;
+                }
+            },
+            stroke: false,
+            fillStyle: color.rgba(255,200,100,0.65),
+            blendMode: 'lighter',
+            phase: i/nr,
+        });
+	
+        
+    }
+}
